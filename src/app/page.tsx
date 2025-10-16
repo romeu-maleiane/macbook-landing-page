3030a718f7430d39254341876d9228f460baf9c6
Romeu Maleiane <romeumaleiane1@gmail.com>
2025-10-16 17:47:33 +0200

    implement showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
 import ProductViewer from "@/components/productViewer";
+import Showcase from "@/components/showcase";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <Navbar />
         <Hero />
         <ProductViewer />
+        <Showcase />
       </main>
   );
 }
